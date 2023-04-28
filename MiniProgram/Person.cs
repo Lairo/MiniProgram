830c645b1d4957b3682e0a28e9823df14fac92a7
chajus1 <chajus1@googlemail.com>
2023-04-28 11:28:54 +0100

    Add project files.

---
+++ MiniProgram/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniProgram
+{
+    internal class Person
+    {
+        public string name;
+        public int age;
+        public bool hasPet;
+
+        public void Greeting()
+        {
+            Console.WriteLine($"Hello my name is {name}, I am {age} years old, and I have a pet");
+        }
+
+        public void Sleep()
+        {
+
+        }
+
+        public void GetPet()
+        {
+
+        }
+
+
+    }
+}
